382a64d6b61541cbb989b16c9c6ff9f7d0ba8358
Zartab Nakhwa <zartab@codewithz.com>
2025-05-21 16:53:46 +0530

    Added the producer demo code

--- cisco-kafka-basics/src/main/java/com/cisco/producer/ProducerDemo.java
+++ cisco-kafka-basics/src/main/java/com/cisco/producer/ProducerDemo.java
@@ -1,2 +1,37 @@
-package com.cisco.producer;public class ProducerDemo {
+package com.cisco.producer;
+
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+
+        final String BOOTSTRAP_SERVERS = "localhost:9092";
+//        final String BOOTSTRAP_SERVERS = "broker-1:29092";
+
+        final String TOPIC_NAME = "cisco-first-topic";
+
+        // Setting the properties for the producer
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+//        Create Kafka Producer Object
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME,  "Hello From Java Producer");
+
+        // Send the record
+        producer.send(record);
+
+        producer.flush();
+
+    }
 }
